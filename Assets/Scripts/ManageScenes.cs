35e20f944cc962b5a429f4700eda3cae043aff95
Cheng Xiong <czxiong@stcloudstate.edu>
2021-03-29 15:59:06 -0500

    Got UI buttons to switch 3d model systems

---
+++ Assets/Scripts/ManageScenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    public void switchSystem(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+}
---
+++ Assets/ZacUIDesign/PanelMain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMain : MonoBehaviour
+{
+    public void SkeletonView()
+    {
+        // Tracks the Skeleton Tracker
+    }
+
+    public void OrganView()
+    {
+        // Tracks the Organ Tracker
+    }
+
+    public void MuscleView()
+    {
+        // Tracks the Muscle Tracker
+    }
+}
