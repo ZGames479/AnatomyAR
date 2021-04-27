3a8b3d2ca707b68b6dcf3d59f002d0e476a0538a
Cheng Xiong <czxiong@stcloudstate.edu>
2021-04-26 21:11:16 -0500

    Added muscle images

---
+++ Assets/Scripts/WorldPositionButton1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WorldPositionButton1 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetTransform;
+
+    private RectTransform rectTransform;
+
+    private Image image;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var screenPoint = Camera.main.WorldToScreenPoint(targetTransform.position);
+        rectTransform.position = screenPoint;
+
+
+    }
+}
